683733e1b5fa5d5b6f848d0b57a206817226ae0d
mahmadmanzoor <62117177+mahmadmanzoor@users.noreply.github.com>
2025-04-05 16:36:23 +0500

    TRPC SETUP

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-export default function Home() {
-  return <div> I will load videos in the future</div>;
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Hello from server" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Error...</div>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+      <div>Server says: Hello from server</div>
+    </HydrateClient>
+  );
 }
