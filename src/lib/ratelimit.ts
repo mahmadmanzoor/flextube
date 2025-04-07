058fe2c22e901f35c170c70e1b38cf957e6ca38f
mahmadmanzoor <62117177+mahmadmanzoor@users.noreply.github.com>
2025-04-07 11:14:46 +0500

    TRPC config

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -2,5 +2,6 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { redis } from "./redis";
 
 export const ratelimit = new Ratelimit({
-    redis,
-    limiter: Ratelimit.slidingWindow(10, "10s"), // 10 requests per minute
+  redis,
+  limiter: Ratelimit.slidingWindow(10, "10s"),
+}); // 10 requests per minute
